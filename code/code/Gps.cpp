468fe533c8ff73153b60ddb8c5c9351f173c5c9c
cub-alan <holwill.jacob@gmail.com>
2026-03-18 14:19:54 +0000

    more comments done

--- code/code/Gps.cpp
+++ code/code/Gps.cpp
@@ -1,109 +1,110 @@
 //Jacob Holwill 10859926
-//
+// the point of this file is to hold the gnss functions for reading and parsing the gnss data and streaming the gnss data 
 
 #include "Gps.hpp"
 
 HardwareSerial GNSS(1);
-GnssData GPS;
+GnssData GPS; // retreive the GnssData struct and give in namespace GPS
 
+//define nessesary pins and baud rate
 #define GNSS_RX 43
 #define GNSS_TX 44
 #define GNSS_BAUD 9600
 
 //========================================================================================================================================================
 
-bool Checksum(const char* GNSS_Data_Line){
-  if (*GNSS_Data_Line != '$') {
-    return false;
+bool Checksum(const char* GNSS_Data_Line){ // a function that will return true or false from the pointer to the gnss line fed in
+  if (*GNSS_Data_Line != '$') { // if the gnss data line does not start with the dolar sign 
+    return false; // feed back false to function
   }
 
-  uint8_t sum = 0;
-  GNSS_Data_Line++;
+  uint8_t sum_check = 0; // create an unsigned 8 bit integer to store the calculated sum
+  GNSS_Data_Line++; // move to the next character after the dollar sign
 
-  while (*GNSS_Data_Line && *GNSS_Data_Line != '*'){
-    sum ^= *GNSS_Data_Line++;
+  while (*GNSS_Data_Line && *GNSS_Data_Line != '*'){ // if it is not the end of the line and stops if it reaches the check sum marker
+    sum_check ^= *GNSS_Data_Line++; //the sum checker is now equal to the XOR of the characters
   }
 
-  if (*GNSS_Data_Line == '*'){
-    int GNSS_get = strtol(GNSS_Data_Line + 1, NULL, 16);
-    return sum == GNSS_get;
+  if (*GNSS_Data_Line == '*'){ // if the end of the line is reached
+    int GNSS_get = strtol(GNSS_Data_Line + 1, NULL, 16);// read the data line including the astrix in base 16
+    return sum_check == GNSS_get; // if the calculated and transmited check sum are equal return true
   }
 
-  return false;
+  return false; // else return false
 }
-
 //============================================================================================================================================================
 
 void Gnss_init(){
-  GPS.mutex = xSemaphoreCreateMutex();
+  GPS.mutex = xSemaphoreCreateMutex(); // create the mutex for the gps
 
-  if (GPS.mutex == NULL) {
-    Serial.println("\n GPS mutex fail");
-    return;
+  if (GPS.mutex == NULL) { // if mutex failed to create
+    Serial.println("\n GPS mutex fail"); 
+    return; // exit the function
   }
 
-  GNSS.begin(GNSS_BAUD, SERIAL_8N1, GNSS_RX, GNSS_TX);
+  GNSS.begin(GNSS_BAUD, SERIAL_8N1, GNSS_RX, GNSS_TX); // begin the GNSS with the set pins and baud rate 
   
-  TaskHandle_t GNSS_Handler = NULL;
-  xTaskCreatePinnedToCore(GnssTask,"GNSS Task",4096,NULL,1,&GNSS_Handler,1);
+  TaskHandle_t GNSS_Handler = NULL; // declair the task required
+  xTaskCreatePinnedToCore(GnssTask,"GNSS Task",4096,NULL,1,&GNSS_Handler,1); //create the gnss task on core 1 (seperate from the camera) with GNSS_Handler as the task
 }
-
 //=====================================================================================================================================================
 
 void GnssTask(void *param){
-  char line[128];
-  int idx = 0;
+  char line[128];// creates a buffer to store the gnss info line in
+  int Current_Point = 0; // creates a variable to hold pointer posision 
 
   while (true){
 
-    while (GNSS.available()){
+    while (GNSS.available()){ // check if the gnss is readable
 
-      char c = GNSS.read();
+      char GNSS_char = GNSS.read(); // create a variable to read the current character in the gnss line
 
-      if (c == '\r') {
-        continue;
+      if (GNSS_char == '\r') { // if its a command to move to the start of terminal
+        continue; // ignore it and move on
       }
 
-      if (c == '\n'){
-        line[idx] = '\0';
-        idx = 0;
+      if (GNSS_char == '\n'){ // if the command is to create a new line
+        line[Current_Point] = '\0'; // add a null terminator to make the c string valid
+        Current_Point = 0; // reset the buffer 
 
-        if (strlen(line) < 10){
-          continue;
+        if (strlen(line) < 10){ //if the length of teh string is less the 10 characters
+          continue; // ignore it and move on
         }
 
-        if (!Checksum(line)){
-          continue;
+        if (!Checksum(line)){ // sends the line through the checksum function 
+          continue; // if checksum is false ignore the line and move on
         }
 
-        if (strncmp(line, "$GNGGA", 6) != 0 && strncmp(line, "$GPGGA", 6) != 0){
-          continue;
+        if (strncmp(line, "$GNGGA", 6) != 0 && strncmp(line, "$GPGGA", 6) != 0){ // checks for the lines with the info i want to receive
+          continue; // if it is a line with the needed info continue the function
         }
         
+        //create an array to store each bit of data that is seperated by commas
         char *fields[15];
         int field = 0;
 
-        char *Line_Split = strtok(line, ",");
+        char *Line_Split = strtok(line, ",");//splits the line by the commas
 
-        while (Line_Split && field < 15){
-          fields[field++] = Line_Split;
-          Line_Split = strtok(NULL, ",");
+        while (Line_Split && field < 15){ // while the are area of the split line left and the feild is less then 15
+          fields[field++] = Line_Split; // take the feild and position and put that section of the line split into the feilds
+          Line_Split = strtok(NULL, ","); // goes to the next section of the split line
         }
 
-        if (field < 10) {
-          continue;
+        if (field < 10) {// ensure there is the right amount of feilds
+          continue; // continue if there are less then 10 feilds
         }
 
-        int quality = atoi(fields[6]);
+        int quality = atoi(fields[6]); // set feild 6 to a quality check
 
-        if (quality == 0){
-          if (xSemaphoreTake(GPS.mutex, portMAX_DELAY)){
+        if (quality == 0){ // if there is no gps signal quality
+          if (xSemaphoreTake(GPS.mutex, portMAX_DELAY)){ // if the mutex is accessable
             GPS.Fix_Val = false;
-            xSemaphoreGive(GPS.mutex);
+            xSemaphoreGive(GPS.mutex); // lock mutex
           }
-          continue;
+          continue; // continue the function
         }
 
+        //set the feilds to their own integers
         const char* latStr = fields[2];
         const char* latDir = fields[3];
         const char* lonStr = fields[4];
@@ -158,11 +159,11 @@ void GnssTask(void *param){
         }
       }
       else{
-        if (idx < sizeof(line) - 1){
-          line[idx++] = c;
+        if (Current_Point < sizeof(line) - 1){
+          line[Current_Point++] = GNSS_char;
         }
         else{
-          idx = 0;
+          Current_Point = 0;
           continue;
         }
       }
@@ -170,7 +171,6 @@ void GnssTask(void *param){
     vTaskDelay(pdMS_TO_TICKS(10));
   }
 }
-
 //===============================================================================================================================================================
 
 esp_err_t GPS_Status_Update(httpd_req_t *Status_Request) { // a static function that returns error for the status request
--- code/code/code.ino
+++ code/code/code.ino
@@ -1,42 +1,42 @@
 //Jacob Holwill 10859926
-//
+//this file is to intergrate my other code together to create the initialisation and loop of my code
 
 #include "MyLib.hpp"
 
 void setup() {
 
-  Serial.begin(115200);
+  Serial.begin(115200); // set the terminal baud rate and begin the terminal
   Serial.setDebugOutput(true);
   for(int i=0;i<40;i++){
-    Serial.println(); // clear the terminal 
+    Serial.println(); // clear 40 lines of the terminal 
   } 
 
-  Gnss_init();
+  Gnss_init(); // initialise the gnss
   Serial.println("GNSS Initialized");
   delay(2000); // Wait 2000 ms
 
-  Cam1_init();
+  Cam1_init(); // initilise the Camera
   Serial.println("CAM Initialized");
-  delay(2000);
+  delay(2000); // Wait 2000 ms
 
-  WIFI_Connect();
+  WIFI_Connect(); // connect to the wifi
   
-  Cam1_Server_Init();
+  Cam1_Server_Init(); // connect the gnss data stream and camera data stream to the wifi
 
   Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
+  Serial.print(WiFi.localIP()); // paste the ip of the stream so it can be opened on the browser
 }
 
 void loop()
 {
-  static unsigned long timer = 0;
+  static unsigned long timer = 0; // create a variable to be able to do non blocking timers
 
-  if (millis() - timer > 1000)
-  {
-    timer = millis();
+  if (millis() - timer > 1000){ // every 1000ms 
+    timer = millis(); // set the millis to the timer to allow for the next loop to run at 1000ms 
 
-    if (xSemaphoreTake(GPS.mutex, pdMS_TO_TICKS(20))){
+    if (xSemaphoreTake(GPS.mutex, pdMS_TO_TICKS(20))){ // if the mutex is readable
       
+      // retreaive the data from the mutex
       bool fix_check = GPS.Fix_Val;
       bool data_check = GPS.data_Received;
 
@@ -44,22 +44,19 @@ void loop()
       double lon = GPS.lon;
       int sats = GPS.satellites;
 
-      if (data_check){
-        GPS.data_Received = false;
+      if (data_check){ //if data has been checked
+        GPS.data_Received = false; // set the data received to false
       }
-      xSemaphoreGive(GPS.mutex);
+      xSemaphoreGive(GPS.mutex); // lock the mutex
 
-      if (fix_check)
-      {
+      if (fix_check){ // if there is a gnss lock then print the data
         Serial.printf("| GPS FIX | LAT: %.6f LON: %.6f SATS: %d\n", lat, lon, sats);
       }
-      else if (GPS.data_Received ||data_check)
-      {
+      else if (GPS.data_Received ||data_check){ // if its receiving lines but not yet got a lock print waiting for fix line
         Serial.println("| GPS COMMUNICATING | Waiting for satellite fix...");
       }
-      else
-      {
-        Serial.println("| GPS NO DATA | Check wiring or GPS power.");
+      else{ // if its not receiving anything show that there is an error
+        Serial.println("| GPS NO DATA | error not receiving");
       }
     }
   }
